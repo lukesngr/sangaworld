ab69659d7992a4eacadd32a584b976be77b7e445
Luke Sanger <lukesngr@gmail.com>
2023-06-30 21:51:07 +1000

    got css grid working

--- src/pages/components/ExpandableCards.js
+++ src/pages/components/ExpandableCards.js
@@ -3,9 +3,16 @@ import Image from "next/image";
 
 function ExpandableCards() {
     return (
-    <div>
-        <Image src="/me.jpg" width={700} height={700} alt="Picture of website" className="profilePicture"/>
-        <div className="windowsDialog"></div>
+    <div className="parent">
+        <div className="vaporDialog">
+            <p>
+            Student at University of Wollongong studying AI and Software Engineering as part of a computer science degree<br />
+            Working at HOYTS as a Service Desk Specialist<br />
+            Keen interest in AI and Web Development<br />
+            Avid lover of Hardstyle and Vaporwave<br />
+            </p>
+        </div>
+        <Image src="/me.jpg" width={700} height={700} alt="Picture of website" className="profilePicture" priority/>
     </div>)
 }
 
