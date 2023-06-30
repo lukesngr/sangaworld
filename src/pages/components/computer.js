95030310b092208ce9e61971f8e17ddd9b16847f
Luke Sanger <lukesngr@gmail.com>
2023-06-30 12:17:26 +1000

    Got code finally rendered

--- src/pages/components/computer.js
+++ src/pages/components/computer.js
@@ -2,11 +2,10 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { useLoader } from "@react-three/fiber"
 function Computer() {
     const pc = useLoader(GLTFLoader, '/computer.glb');
-    return (
-        <group>
-            <primitive object={pc.scene}></primitive>
-        </group>
-    )
+    pc.scene.position.y = -6;
+    pc.scene.rotation.y = 4.75;
+    pc.scene.position.z = -40;
+    return <primitive object={pc.scene}></primitive>
 }
 
 export default Computer
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,52 @@
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useLoader, useThree } from "@react-three/fiber";
 import Computer from "./components/computer";
-import { useEffect } from "react";
+import { Suspense, useEffect } from "react";
+import { TextureLoader } from "three/src/loaders/TextureLoader";
+import { useState } from "react";
+
+function Background() {
+    const { scene } = useThree();
+    const texture = useLoader(TextureLoader, '/0_0.png');
+    useEffect(() => {
+      if (!texture) return;
+  
+      const imageAspect = texture.image.width / texture.image.height;
+      texture.matrixAutoUpdate = false;
+      const aspect = window.innerWidth / window.innerHeight;
+      
+      if (aspect < imageAspect) {
+        texture.matrix.setUvTransform(0, 0, aspect / imageAspect, 1, 0, 0.5, 0.5);
+      } else {
+        texture.matrix.setUvTransform(0, 0, 1, imageAspect / aspect, 0, 0.5, 0.5);
+      }
+      
+      scene.background = texture;
+    }, [scene, texture]);
+    
+    return null;
+  }
 
 function LandingPage() {
-    let aspectRatio = 0;
-    let dpr = 0;
-    useEffect(function(){
-        aspectRatio = window.innerWidth / window.innerHeight;
-        dpr = window.devicePixelRatio;
-        
-    })
+    const [dpr, setDpr] = useState(1);
+    const [aspectRatio, setAspectRatio] = useState(1);
+
+    useEffect(() => {
+        setDpr(window.devicePixelRatio);
+        setAspectRatio(window.innerWidth / window.innerHeight);
+    }, []);
+    
     return (
-        <Canvas camera={{fov: 75, position: [0, 0, 30], aspect: aspectRatio}} dpr={dpr}>
-            <ambientLight></ambientLight>
-            <pointLight color="0xf59df5" position={[0, 4.75, -20]}></pointLight>
-            <Computer></Computer>
-        </Canvas>
+        <div style={{width: "100vw", height: "100vh"}}>
+            <Canvas camera={{fov: 75, position: [0, 0, 30], aspect: aspectRatio}} dpr={dpr}>
+                <Suspense fallback={null}>
+                    <Background></Background>
+                    <Computer></Computer>
+                </Suspense>
+                <ambientLight></ambientLight>
+                <pointLight color={0xf59df5} position={[0, 4.75, -20]}></pointLight>
+                
+            </Canvas>
+        </div>
     )
 }
 
