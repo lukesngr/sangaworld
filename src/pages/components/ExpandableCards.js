9ed781547a7f2e1e10b96d85a02f14eb5e0556bb
Luke Sanger <lukesngr@gmail.com>
2023-06-30 22:18:18 +1000

    Design of win knockoff dialog done

--- src/pages/components/ExpandableCards.js
+++ src/pages/components/ExpandableCards.js
@@ -5,7 +5,10 @@ function ExpandableCards() {
     return (
     <div className="parent">
         <div className="vaporDialog">
-            <p className="dialContent">
+            <div className="dialogTitle">
+                About Me <span className="fakeExitButton">X</span>
+            </div>
+            <p className="dialogContent">
             Student at University of Wollongong studying AI and Software Engineering as part of a computer science degree<br />
             Working at HOYTS as a Service Desk Specialist<br />
             Keen interest in AI and Web Development<br />
