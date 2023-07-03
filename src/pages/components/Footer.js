23a2298ab80ab85225df2116a55f29e09940cddc
Luke Sanger <lukesngr@gmail.com>
2023-07-03 21:22:22 +1000

    Responsive top down menu made

--- src/pages/components/Footer.js
+++ src/pages/components/Footer.js
@@ -2,7 +2,7 @@ function Footer() {
     const divStyle = {
         width: '100%',
         height: '5em',
-        backgroundColor: 'black',
+        backgroundColor: '#242321',
     }
     return <div style={divStyle}></div>
 }
--- src/pages/components/NavbarWithBanner.js
+++ src/pages/components/NavbarWithBanner.js
@@ -1,25 +1,56 @@
 import '../../styles/navbar.css';
 import "../../styles/banner.css";
 import { FaBars } from "react-icons/fa";
+import { useState } from 'react';
 
 
 function NavbarWithBanner(props) {
     let itemStyles = ["firstItem navItem", "navItem", "navItem"];
     itemStyles[props.current] += " currentPage";
-    return (
-    <>
-        <nav>
-            <ul className="navList">
-                <li className={itemStyles[0]}><a href="/home">Home</a></li>
-                <li className={itemStyles[1]}><a href="/tools">Tools</a></li>
-                <li className={itemStyles[2]}><a href="/blog">Blog</a></li>
-            </ul>
-        </nav>
-        <div className="bannerDiv">
-            <FaBars className="text-left" size={40} fill="white"/>
-            <h1 className="text-center">SangaWorld</h1>
-        </div>
-    </>)
+    const [navClass, setNavClass] = useState("normal")
+
+    function showTopDownMenu() {
+        if(navClass == "normal") {
+            setNavClass("responsive");
+        }else{
+            setNavClass("normal");
+        }
+    }
+
+    if(navClass == "responsive") {
+        return (
+            <>
+                <div className="bannerDiv">
+                    <FaBars className="text-left mobileMenuButton" onClick={showTopDownMenu} size={30} fill="#7acaff"/>
+                    <h1>SangaWorld</h1>
+                </div>
+                <nav className={navClass}>
+                    <ul className="navList">
+                        <li className={itemStyles[0]}><a href="/home">Home</a></li>
+                        <li className={itemStyles[1]}><a href="/tools">Tools</a></li>
+                        <li className={itemStyles[2]}><a href="/blog">Blog</a></li>
+                    </ul>
+                </nav>
+            </>)
+
+    }else{
+        return (
+            <>
+                <nav className={navClass}>
+                    <ul className="navList">
+                        <li className={itemStyles[0]}><a href="/home">Home</a></li>
+                        <li className={itemStyles[1]}><a href="/tools">Tools</a></li>
+                        <li className={itemStyles[2]}><a href="/blog">Blog</a></li>
+                    </ul>
+                </nav>
+                <div className="bannerDiv">
+                    <FaBars className="text-left mobileMenuButton" onClick={showTopDownMenu} size={30} fill="white"/>
+                    <h1>SangaWorld</h1>
+                </div>
+            </>)
+    }
+
+    
 }
 
 export default NavbarWithBanner;
