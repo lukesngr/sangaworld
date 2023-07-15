bf6e6a067564624a6cd809a1706651a06c563f32
Luke Sanger <lukesngr@gmail.com>
2023-07-15 11:19:18 +1000

    starting to make design better

--- src/components/home/ExpandableCards.js
+++ src/components/home/ExpandableCards.js
@@ -1,27 +1,25 @@
 import { useState } from "react";
 import "../../styles/expandablecard.scss";
 import Image from "next/image";
+import { FaTimesCircle } from "react-icons/fa";
 
 function ExpandableCards() {
 
-    const [displayOptions, setDisplayOptions] = useState({display: "none"});
-
     return (
-    <div className="parent">
-        <div className="vaporDialog">
-            <div className="dialogTitle">
-                About Me <button className="fakeExitButton">X</button>
-            </div>
-            <p className="dialogContent">
-            Hi, I am Luke.
-            I am currently a Student at University of Wollongong studying AI and Software Engineering as part of a computer science degree<br />
-            I am working at HOYTS as a Service Desk Specialist and have been for over 3 years<br />
-            Keen interest in AI and Web Development<br />
-            Avid lover of Hardstyle and Vaporwave music<br />
-            </p>
+    <div className="vaporDialog">
+        <div className="dialogTitle">
+            About Me <FaTimesCircle className="fakeExitButton" size={30} />
         </div>
         <Image src="/me.jpg" width={700} height={700} alt="Picture of website" className="profilePicture" priority/>
-    </div>)
+        <p className="dialogContent">
+        Hi, I am Luke.
+        I am currently a Student at University of Wollongong studying AI and Software Engineering as part of a computer science degree<br />
+        I am working at HOYTS as a Service Desk Specialist and have been for over 3 years<br />
+        Keen interest in AI and Web Development<br />
+        Avid lover of Hardstyle and Vaporwave music<br />
+        </p>
+    </div>    
+    )
 }
 
 export default ExpandableCards;
