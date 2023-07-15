f02b6698e1a3a388cd7ca316ba381b7115ffa1c9
Luke Sanger <lukesngr@gmail.com>
2023-07-15 16:32:15 +1000

    Added links to footer

--- src/components/pageParts/Footer.js
+++ src/components/pageParts/Footer.js
@@ -1,12 +1,14 @@
+import { FaLinkedin, FaGithub, FaMedium } from "react-icons/fa";
+import '../../styles/footer.scss';
+
 function Footer() {
-    const divStyle = {
-        width: '100%',
-        height: '5em',
-        backgroundColor: '#242321',
-        position: 'fixed',
-        bottom: 0
-    }
-    return <div style={divStyle}></div>
+    const ICON_SIZE = 50;
+    return (
+    <div className="hstack gap-3 footerDiv">
+        <a className="linkedinLink" href="https://www.linkedin.com/in/luke-sanger-43b33420b/"><FaLinkedin size={ICON_SIZE}></FaLinkedin></a>
+        <a className="githubLink"  href="https://github.com/lukesngr"><FaGithub size={ICON_SIZE}></FaGithub></a>
+        <a className="mediumLink"  href="https://medium.com/@lukesngr/about"><FaMedium size={ICON_SIZE}></FaMedium></a>
+    </div>)
 }
 
 export default Footer;
