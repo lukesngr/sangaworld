602772e3f7546b0e45128a9e23e1b9e5bfcc7726
Luke Sanger <lukesngr@gmail.com>
2023-07-25 08:54:36 +1000

    Favicon icon added again

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,6 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <link rel="icon" type="image/x-icon" href="/favicon.ico"></link>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
       <body className={inter.className}>{children}</body>
