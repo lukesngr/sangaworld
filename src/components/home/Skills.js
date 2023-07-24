0d7a33d6d56f17f14a53f600253306b0b038962e
Luke Sanger <lukesngr@gmail.com>
2023-07-25 09:07:26 +1000

    Header changed

--- src/components/home/Skills.js
+++ src/components/home/Skills.js
@@ -10,7 +10,7 @@ function Skills() {
             </div>
             <div className="row">
                 <div className="col-md workSkills">
-                    Throughout my employment at HOYTS, I have learned numerous skills including but not limited to
+                    Skills Acquired During Employment at HOYTS
                     <ul>
                         <li>Teamwork</li>
                         <li>Powershell</li>
@@ -22,16 +22,16 @@ function Skills() {
                     </ul>
                 </div>
                 <div className="col-md uniSkills">
-                I am on my final year of a 3 year degree in Computer Science, the skills I have learnt through this include
-                <ul>
-                    <li>Java</li>
-                    <li>C++</li>
-                    <li>NextJs</li>
-                    <li>HTML, CSS and Javascript</li>
-                    <li>React</li>
-                    <li>Machine learning</li>
-                    <li>Neural Networks</li>
-                </ul>
+                    Skills Developed During My 3-Year Computer Science Degree:
+                    <ul>
+                        <li>Java</li>
+                        <li>C++</li>
+                        <li>NextJs</li>
+                        <li>HTML, CSS and Javascript</li>
+                        <li>React</li>
+                        <li>Machine learning</li>
+                        <li>Neural Networks</li>
+                    </ul>
                 </div>
             </div>
         </div>
