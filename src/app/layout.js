c88d1536f359e8490ded8b2939ff28e18b3fa12f
Luke Sanger <lukesngr@gmail.com>
2023-07-25 08:42:42 +1000

    Favicon added to website

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
       <body className={inter.className}>{children}</body>
