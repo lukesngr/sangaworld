aaf89a7a29b353dc25add3f68761ffe31f9eb193
Luke Sanger <lukesngr@gmail.com>
2023-07-24 21:48:54 +1000

    Added icon

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,6 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
       <body className={inter.className}>{children}</body>
