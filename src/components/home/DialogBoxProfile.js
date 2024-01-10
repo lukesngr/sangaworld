30855af07d10b592aa638c69a2bc0601203c0407
Luke Sanger <lukesngr@gmail.com>
2024-01-10 13:33:00 +1100

    Think I got drag and drop working for window

--- src/components/home/DialogBoxProfile.js
+++ src/components/home/DialogBoxProfile.js
@@ -1,21 +1,48 @@
 import "../../styles/dialogboxprofile.scss";
 import Image from "next/image";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { FaTimesCircle, FaMinusCircle } from "react-icons/fa";
 
 function DialogBoxProfile() {
 
+    const [transformX, setTransformX] = useState(0);
+    const [transformY, setTransformY] = useState(0);
+    const [mouseDown, setMouseDown] = useState(false);
+    const [mouseX, setMouseX] = useState({old: 0, new: 0});
+    const [mouseY, setMouseY] = useState({old: 0, new: 0});
+
+    useEffect(() => {
+        const updateCoordinates = (e) => {
+            setMouseX((previousValue) => {return {old: previousValue.new, new: e.x}});
+            setMouseY((previousValue) => {return {old: previousValue.new, new: e.y}});
+        }
+        window.addEventListener('mousemove', updateCoordinates);
+        return () => {
+            window.removeEventListener('mousemove', updateCoordinates);
+        }
+    }, []);
+
+    console.log(mouseX, mouseY);
+
+    useEffect(() => {
+        if(mouseDown) {
+            setTransformX((mouseX.new - mouseX.old)+transformX);
+            setTransformY((mouseY.new - mouseY.old)+transformY);
+        }
+    }, [mouseX, mouseY]);
+
+
     return (
-    <div className="vaporDialog">
-        <div className="dialogTitle">
+    <div style={{'transform': `translate(${transformX}px, ${transformY}px)`}} className="vaporDialog">
+        <div onMouseDown={() => setMouseDown(true)} onMouseUp={() => setMouseDown(false)} className="dialogTitle">
             About Me <FaTimesCircle className="fakeExitButton" size={30} /><FaMinusCircle className="fakeMinusButton" size={30} />
         </div>
         <Image src="/me.jpg" width={700} height={700} alt="Picture of website" className="profilePicture" priority/>
         <p className="dialogContent">
-        Greetings! My name is Luke Sanger, an eager student currently pursuing a degree in Computer Science with a focus on AI and Software Engineering at the esteemed University of Wollongong. <br />
-        For over three years, I have honed my IT skills in a real-world setting at HOYTS, where I hold the position of a Service Desk Specialist. <br />
-        My interests aren't just limited to academia and professional work - I also have a keen enthusiasm for AI and Web Development that goes beyond the classroom. <br />
-        Outside of tech, I am an ardent fan of Hardstyle and Vaporwave music genres. <br />
+        My name is Luke Sanger, I have just finished a degree in Computer Science at University of Wollongong. <br />
+        For over five years, I have honed my IT skills in a real-world setting at HOYTS, where I hold the position of a Service Desk Specialist. <br />
+        My life isn't just limited to university and professional work - I also have a keen enthusiasm for AI and Web Development that goes beyond the classroom. <br />
+        Outside of tech, I am an ardent fan of Hardstyle and Vaporwave music genres and love to go to HSU events. <br />
         </p>
         <a href="/myresume.pdf" className="downloadResumeButton" target='_blank' rel='noopener noreferrer'>Download Resume</a>
     </div>
