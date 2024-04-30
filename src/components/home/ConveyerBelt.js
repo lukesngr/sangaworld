73d6ec49075e1b9526590b4745a8effea3e3a523
Luke Sanger <lukesngr@gmail.com>
2024-04-30 20:36:56 +1000

    Distance working, prehaps time to do animation

--- src/components/home/ConveyerBelt.js
+++ src/components/home/ConveyerBelt.js
@@ -3,6 +3,9 @@ import '../../styles/conveyer.scss'
 export default function ConveyerBelt() {
     return (
     <div className="scene">
-        <img src="/conveyerbelt.png" className='conveyerBelt' alt='conveyer belt'/>
+        <div className="conveyerBeltSection">
+            <img src="/chute.png" className='chute' alt='chute'/>
+            <img src="/conveyerbelt.png" className='conveyerBelt' alt='conveyer belt'/>
+        </div>
     </div>)
 }
