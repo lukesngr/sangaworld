3b13cd984b710a3f9ca45e46b9090717d593436c
Luke Sanger <lukesngr@gmail.com>
2024-05-01 20:12:16 +1000

    Looking good so far just need to work out responsive indent

--- src/components/home/Hiring.js
+++ src/components/home/Hiring.js
@@ -5,7 +5,7 @@ export default function Hiring() {
         <div className="hiringDiv">
             <h1 className="hiringHeader">Contact</h1>
             <p className="hiringText">
-                If you are interested in hiring me you can find my resume <a href="/resume.pdf">here</a>
+                If you are interested in hiring me you can find my resume <a href="/resume.pdf">here</a> <br />
                 For any other enquiries, please email me at lukesngr@gmail.com
             </p>
         </div>
