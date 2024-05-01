4e3b67d47cdd9efa6fbef4042ad4dbe8f958731b
Luke Sanger <lukesngr@gmail.com>
2024-05-01 21:41:53 +1000

    hopefully this fixes issue

--- src/pages/home.js
+++ src/pages/home.js
@@ -12,6 +12,9 @@ function Home() {
     
     return (<>
     <link rel="preload" href="/FoxCavalier.tff" as="font" type="font/tff" crossOrigin></link>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400..700&display=swap" rel="stylesheet"></link>
     <Meta siteTitle="Homepage" description="Homepage with summary of LUke Sanger's professional summary,  skills and contact points"></Meta>
     <NavbarWithBanner current={0} />
     <DialogBoxContextProvider>
