3fe8d3754195b34af9e11842c5b877caf27096f4
Luke Sanger <lukesngr@gmail.com>
2024-05-01 12:25:05 +1000

    Style slowly getting better

--- src/components/home/MyStory.js
+++ src/components/home/MyStory.js
@@ -1,5 +1,13 @@
 import '../../styles/mystory.scss';
 
 export default function MyStory() {
-    return <div className="myStory"></div>
+    return <>
+    <div className="myStory">
+        <div className='intakePipe'>
+        </div>
+        <div class="pipeOutlet"></div>
+        <div className='underLine'>
+        </div>
+        <div className='dotPoints'></div>
+    </div></>
 }
